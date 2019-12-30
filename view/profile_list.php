45274becee4c0edc769c2bfbc545472a41750a8c
KasraTabrizi <kasra_tabrizi@hotmail.com>
2019-12-30 16:00:06 +0100

    profile_list page HTML done

---
+++ view/profile_list.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div id="wrapper">
+        <h1>List of Profiles</h1>
+
+        <div id="container-profilelist">
+            <!-- table comes here -->
+        </div>
+    </div>
+</body>
+</html>
