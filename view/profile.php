77952dde58959c0fe17890765df21ef57347fd30
KasraTabrizi <kasra_tabrizi@hotmail.com>
2019-12-30 15:54:07 +0100

    profile page HTML done

---
+++ view/profile.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div id="wrapper">
+        <h1>Profile Page</h1>
+
+        <div id="container_profile">
+            <div id="profile-img">
+                <img src="" alt="">
+            </div>
+            <div id="profile-description">
+                <table>
+                </table>
+            </div>
+            <form action="" method="POST">
+                <input type="button" name="edit" value="Edit">
+                <input type="button" name="logout" value="Log Out">
+                <input type="button" name="backtolist" value="Back">
+            </form>
+        </div>
+    </div>
+</body>
+</html>
