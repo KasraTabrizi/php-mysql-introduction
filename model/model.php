009c2fd0f9eb7958ba298aea3c0b414bf4e72937
KasraTabrizi <kasra_tabrizi@hotmail.com>
2019-12-30 16:16:48 +0100

    refactored sendToDatabase

--- model/model.php
+++ model/model.php
@@ -22,12 +22,10 @@
         $data = "INSERT INTO student (first_name, last_name, username, gender, linkedin, github, email, preferred_language, avatar, video, quote, quote_author, created_at)
                         VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
         $stmt = $database->prepare($data);
-        $stmt->execute([$_POST['first_name'],$_POST['last_name'],$_POST['username'],$_POST['gender'],$_POST['linkedin'],$_POST['github'],$_POST['email'],$_POST['preferred_language'],$_POST['avatar'],$_POST['video'],$_POST['quote'],$_POST['quote_author'],$_POST['created_at']]);
-
-        if ($database->prepare($data) === FALSE) {
+        if ($stmt === FALSE) {
             echo "Error: " . $data . "<br>" . $database->errorInfo();
         } else {
-            echo "New record created successfully";
+            $stmt->execute([$_POST['first_name'],$_POST['last_name'],$_POST['username'],$_POST['gender'],$_POST['linkedin'],$_POST['github'],$_POST['email'],$_POST['preferred_language'],$_POST['avatar'],$_POST['video'],$_POST['quote'],$_POST['quote_author'],$_POST['created_at']]);
         }
     }
 
