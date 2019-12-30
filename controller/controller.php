088bcaf0f95b820ddac4891e34b8f4403ea53f12
KasraTabrizi <kasra_tabrizi@hotmail.com>
2019-12-30 16:05:14 +0100

    controller.php contains all the if for the buttons

---
+++ controller/controller.php
@@ -0,0 +1,27 @@
+<?php
+
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+    if (isset($_POST['login'])) {
+        
+    }
+    elseif(isset($_POST['signup'])) {
+        
+    }
+    elseif(isset($_POST['register'])) {
+        
+    }
+    elseif(isset($_POST['edit'])) {
+        
+    }
+    
+    elseif(isset($_POST['logout'])) {
+        
+    }
+    elseif(isset($_POST['backtolist'])) {
+        
+    }
+
+}
+
+?>
