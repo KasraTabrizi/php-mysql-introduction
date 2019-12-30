dc8124717c359d657017f3052edfe7ba309df951
KasraTabrizi <kasra_tabrizi@hotmail.com>
2019-12-30 16:25:57 +0100

    created ifInputEmpty to check for empty input text fields

--- model/model.php
+++ model/model.php
@@ -19,13 +19,20 @@
 
     //SEND DATA FROM REGISTRATION FORM TO DATABASE
     function sendToDatabase($database, $data){
-        $data = "INSERT INTO student (first_name, last_name, username, gender, linkedin, github, email, preferred_language, avatar, video, quote, quote_author, created_at)
+        $sql = "INSERT INTO student (first_name, last_name, username, gender, linkedin, github, email, preferred_language, avatar, video, quote, quote_author, created_at)
                         VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-        $stmt = $database->prepare($data);
+        $stmt = $database->prepare($sql);
         if ($stmt === FALSE) {
-            echo "Error: " . $data . "<br>" . $database->errorInfo();
+            echo "Error: " . $sql . "<br>" . $database->errorInfo();
         } else {
             $stmt->execute([$_POST['first_name'],$_POST['last_name'],$_POST['username'],$_POST['gender'],$_POST['linkedin'],$_POST['github'],$_POST['email'],$_POST['preferred_language'],$_POST['avatar'],$_POST['video'],$_POST['quote'],$_POST['quote_author'],$_POST['created_at']]);
+        }
+    }
+
+    //CHECK IF ANY TEXT INPUT IS EMPTY OR NOT
+    function ifInputEmpty($inputName){
+        if(empty($inputName)){
+            return "Field is empty";
         }
     }
 
