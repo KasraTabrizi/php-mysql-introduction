2f3614d1d6fd687e416e16320f8f47b759c9cdf6
KasraTabrizi <kasra_tabrizi@hotmail.com>
2020-01-03 12:31:17 +0100

    styled login page part 1

--- view/index.php
+++ view/index.php
@@ -5,23 +5,42 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="../styles/css/index.css">
 </head>
 <?php 
     require '../controller/controller.php'; 
 ?>
 <body>
     <div id="wrapper">
-        <h1>Title</h1>
-
-        <div id="container-login">
-            <form action="" method="POST">
-                <label for="email">Email:<?php echo $errorMail; ?></label>
-                <input type="text" name="email" id="email" placeholder="example@example.com">
-                <label for="email">Password:<?php echo $errorPass; ?></label>
-                <input type="password" name="password_login" id="password" placeholder="password">
-                <input type="submit" name="login" value="Log In">
-                <input type="submit" name="signup" value="Sign Up">
-            </form>
+        <div id="main-container">
+            <div id="left-container">
+                <div id="text-layer">
+                    <h1>Title</h1>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem voluptatum aspernatur excepturi nemo incidunt eveniet, quibusdam alias omnis placeat reiciendis ab numquam dolores repudiandae quia blanditiis iusto iste molestiae ea.</p>
+                </div>
+            </div> 
+            <div id="login-container">
+                <form action="" method="POST">
+                    <table>
+                        <tr >
+                            <td colspan="2"><label for="email">Email:<?php echo $errorMail;?></label></td>
+                        </tr>
+                        <tr>
+                            <td colspan="2"><input type="text" name="email" id="email" placeholder="example@example.com"></td>
+                        </tr>
+                        <tr>
+                            <td colspan="2"><label for="email">Password:<?php echo $errorPass;?></label></td>
+                        </tr>
+                        <tr>
+                            <td colspan="2"><input type="password" name="password_login" id="password" placeholder="password"></label></td>
+                        </tr>
+                        <tr>
+                            <td><input type="submit" name="login" value="Log In"></td>
+                            <td><input type="submit" name="signup" value="Sign Up"></td>
+                        </tr>
+                    </table>
+                </form>
+            </div>
         </div>
     </div>
 </body>
