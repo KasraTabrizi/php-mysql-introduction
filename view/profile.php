b2fc4afc699428dbc07eaaa87bfba15ec97496c6
KasraTabrizi <kasra_tabrizi@hotmail.com>
2020-01-03 16:59:04 +0100

    profile styling partially done

--- view/profile.php
+++ view/profile.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link href="https://fonts.googleapis.com/css?family=Rubik:300,300i,400,400i,500,500i,700,700i,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../styles/css/profile.css">
 </head>
 <?php
     require "../controller/controller.php";
@@ -12,16 +14,17 @@
 
 <body>
     <div id="wrapper">
-        <h1>Profile Page</h1>
+        <!-- <h1>Profile Page</h1> -->
         <div id="container_profile">
             <div id="profile-img">
-                <img class="gender-icon" src="" alt="gender-icon">
-                <img class="profile-image" src="" alt="profile-image">
+                <img class="profile-image" src="<?php echo $_SESSION['profile']['avatar'];?>" alt="gender-icon">
+                <img class="gender-icon" src="https://img.icons8.com/cotton/60/000000/male.png" alt="profile-image">
             </div>
             <p class="user-name"><?php echo $_SESSION['profile']['username'];?></p>
             <p class="quote"><?php echo $_SESSION['profile']['quote'];?> ~ <?php echo $_SESSION['profile']['quote_author'];?> </p>
-            <div id="profile-description">
-                <table>
+        </div>
+        <div id="profile-description">
+            <table>
                     <tr>
                         <td>First Name</td>
                         <td><?php echo $_SESSION['profile']['first_name'];?></td>
@@ -39,16 +42,18 @@
                         <td><?php echo $_SESSION['profile']['email'];?></td>
                     </tr>
                 </table>
-            </div>
+            
             <ul id="social-media">
-                <li><a href=""><img class="social-icon" src="" alt=""></a></li>
-                <li><a href=""><img class="social-icon" src="" alt=""></a></li>
-                <li><a href=""><img class="social-icon" src="" alt=""></a></li>
+                <li><a href=""><img src="https://img.icons8.com/color/48/000000/linkedin.png"></a></li>
+                <li><a href=""><img src="https://img.icons8.com/color/48/000000/github--v1.png"></a></li>
+                <li><a href=""><img src="https://img.icons8.com/color/48/000000/youtube.png"></a></li>
             </ul>
+        </div>
+        <div id="buttons-container">
             <form action="" method="POST">
-                <input type="submit" name="edit" value="Edit">
-                <input type="submit" name="logout" value="Log Out">
-                <input type="submit" name="backtolist" value="Home">
+                <input type="submit" name="edit" value="Edit" id="edit-button">
+                <input type="submit" name="logout" value="Log Out" id="logout-button">
+                <input type="submit" name="backtolist" value="Home" id="home-button">
             </form>
         </div>
     </div>
